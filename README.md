9d34fc92764db9d1311223db49e189f59e8c073f
vd1992 <40485260+vd1992@users.noreply.github.com>
2019-01-16 23:44:11 -0700

    Big work, learning about State

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # react-adding-on
+learning bits and pieces
 
 https://vd1992.github.io/react-adding-on/index.html
--- react_script.js
+++ react_script.js
@@ -1,11 +1,74 @@
+////////////////////
+//toggle component
+///////////////////
 class ReactBit extends React.Component {
-      
-    render() {
-      return (
-        <h1>This is a React Component using JSX</h1>
-      );
+  //initialize state
+  constructor(props) {
+    super(props);
+    this.state = {toggle: "off"};
+  } 
+
+  //method that when called, toggles
+  button_switch(){
+    if(this.state.toggle=="off"){
+      this.setState({
+        toggle: "on"
+      });
+      document.getElementById("react-app1").style.color="green";
+    }
+    else{
+      this.setState({
+        toggle: "off"
+      });
+      document.getElementById("react-app1").style.color="black";
     }
   }
-  
-  const holder = document.querySelector('#react-app');
-  ReactDOM.render(<ReactBit/>, holder);
+
+  //render to DOM, div containing button with bound function and the text that is updated
+  render() {
+      return (
+        <div id="states-div">
+          <div id="button"><button onClick={this.button_switch.bind(this)}>Click Me</button></div>
+          <div id="react-app1">The toggle state is {this.state.toggle}</div>
+        </div>
+      );
+  }
+}
+
+//acquire holding and render
+const holder = document.querySelector('#states-div');
+ReactDOM.render(<ReactBit/>, holder);
+
+//////////////////////////
+//clock component
+//////////////////////////
+class Clock extends React.Component{
+  //initialize current time
+  constructor(props){
+    super(props);
+    this.state = {time:new Date()};
+  }
+
+  //method to update state with new time
+  updateTime(){
+    this.setState({
+      time:new Date()
+    });
+  }
+
+  //lifecycle method that triggers on render, an interval function to keep updating every second
+  componentDidMount(){
+    setInterval(this.updateTime.bind(this),1000);
+  }
+
+  //render string containing time in the current state
+  render(){
+    return(
+      <p id="the-time">The time is now {this.state.time.toLocaleTimeString()}</p>
+    );
+  }
+}
+
+//get hold and render clock
+const holder2 = document.getElementById("clock");
+ReactDOM.render(<Clock/>, holder2);
